ae9aa41bf07e5a566eac02af201ea5f159de4e70
vijaya <vijaya.venkatachalam@nomadrail.com>
2020-08-07 19:44:35 +0100

    added another route for hello-name

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/<name>')
+def hello_name(name):
+    print(name)
+    nameStr = "Hello %s" % name
+    return nameStr
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000',debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
